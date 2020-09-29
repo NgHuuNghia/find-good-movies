08526b1306b3c728d2c97d32828126f611e2b2d8
Nguyễn Hữu Nghĩa <nguyenhuunghia152@gmail.com>
2020-09-29 17:22:04 +0700

    import images es6 config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withImages = require("next-images");
-module.exports = withImages();
-
-// module.exports = {
-//   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-//   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-// };
+module.exports = withImages({
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  dynamicAssetPrefix: true,
+  webpack(config, options) {
+    return config;
+  },
+});
